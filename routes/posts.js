78a45b9bee7562e05ff4ed4ff75fd0ce02371de1
nickmly <nickmly@gmail.com>
2017-11-16 11:33:07 -0500

    Tabbed post forms

--- routes/posts.js
+++ routes/posts.js
@@ -4,8 +4,13 @@ var Post = require('../models/post.js');
 
 
 
-router.get("/post/new", function(req,res){
-    res.render("post/new");
+router.get("/post/:post_id", function(req,res){
+    Post.findById(req.params.post_id, function(err,post){
+        if(err)
+            return console.log(err);
+        res.render("post/show",{post: post});
+    });
+    
 });
 
 router.post("/post", function(req,res){
