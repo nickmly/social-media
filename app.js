2006c5e187f6bef1e4f7d8fcf7b79a64acd3ed03
nickmly <nickmly@gmail.com>
2017-11-15 19:10:49 -0500

    Setup requirements

---
+++ app.js
@@ -0,0 +1,36 @@
+////////////////////////////////
+// REQUIREMENTS
+////////////////////////////////
+
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser')
+    passport = require('passport'),
+    LocalStrategy = require('passport-local');
+
+////////////////////////////////
+////////////////////////////////
+
+// Use body parser
+app.use(bodyParser.urlencoded({extended: true}));
+// Use public directory 
+app.use(express.static(__dirname + "/public"));
+// So we don't have to type in .ejs for every res.render call
+app.set("view engine", "ejs");
+
+
+////////////////////////////////
+// ROUTES
+////////////////////////////////
+app.get("/", function(req,res){
+    res.render("landing");
+});
+
+////////////////////////////////
+////////////////////////////////
+
+// Listen on port 3000
+app.listen(3000, process.env.IP, function(){
+    console.log("Server has started");
+});
