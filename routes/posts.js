50656f9bad678080c8d736f95cf76a0a39b9fe67
nickmly <nickmly@gmail.com>
2017-11-16 00:07:24 -0500

    Submit posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Post = require('../models/post.js');
+
+
 
 router.get("/post/new", function(req,res){
     res.render("post/new");
 });
 
 router.post("/post", function(req,res){
-    console.log(req.body);
-    res.send("Created new post");
+    var title = req.body.title;
+    var link = req.body.link;
+    var content = req.body.content;
+    Post.create({
+        title: title,
+        link: link,
+        content: content
+    }, function(err, post){
+        if(err)
+            return console.log(err);
+        res.redirect("/");
+    });
+    
 })
 
 module.exports = router;
