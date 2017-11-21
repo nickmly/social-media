20fbcdc6822d70feffbba8344978effb406a1f3f
nickmly <nickmly@gmail.com>
2017-11-20 20:52:10 -0500

    Posts update

--- routes/posts.js
+++ routes/posts.js
@@ -49,7 +49,7 @@ router.post("/post", isLoggedIn, function(req,res){
 
 // LIKED A POST
 router.get("/post/:post_id/like", function(req,res){
-    console.log(req.query);
+    
     Post.findById(req.params.post_id, function(err,post){
         if(err) {
             console.log("Failed to like post: " + err);
